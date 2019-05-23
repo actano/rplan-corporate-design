d69571e743d9c8c5452ad9e22bd4faabe47fb8c7
Hendrik Purmann <h.purmann@googlemail.com>
2019-05-23 15:14:52 +0200

    CON-202: Style errorDialog component

--- src/components/error-dialog.jsx
+++ src/components/error-dialog.jsx
@@ -1,32 +1,38 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import classNames from 'classnames'
 import withStyles from '@material-ui/core/styles/withStyles'
 
 import Paper from '@material-ui/core/Paper'
 import Typography from '@material-ui/core/Typography'
 
-import { Wall } from './wall'
-
 const styles = theme => ({
   errorDialog: {
+    width: '100%',
     backgroundColor: theme.palette.error.light,
+    border: `1px solid ${theme.palette.error.main}`,
     textAlign: 'center',
+    padding: '24px',
+    boxShadow: 'none',
   },
 })
 
-const _ErrorDialog = ({ classes, children }) => (
-  <Paper className={classes.errorDialog}>
-    <Wall size="small">
-      <Typography variant="body2">
-        { children }
-      </Typography>
-    </Wall>
+const _ErrorDialog = ({ classes, className, children }) => (
+  <Paper className={classNames(classes.errorDialog, className)}>
+    <Typography variant="h5">
+      { children }
+    </Typography>
   </Paper>
 )
 
 _ErrorDialog.propTypes = {
   classes: PropTypes.object.isRequired,
+  classNames: PropTypes.string,
   children: PropTypes.node.isRequired,
+}
+
+_ErrorDialog.defaultProps = {
+  classNames: '',
 }
 
 const ErrorDialog = withStyles(styles)(_ErrorDialog)
