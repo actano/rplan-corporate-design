9c2186745f2c8b2969179e36e7d2cec2bd825054
Hendrik Purmann <h.purmann@googlemail.com>
2019-07-25 14:44:52 +0200

    PA-969: Add example of different dot menu styling
    This shows that the button can be customized (as in the focus card).

--- storybook/stories/dotmenu/index.jsx
+++ storybook/stories/dotmenu/index.jsx
@@ -1,8 +1,40 @@
 import React from 'react'
-import { Grid, Typography } from '@material-ui/core'
+import { Grid, Typography, makeStyles } from '@material-ui/core'
 import { storiesOf } from '@storybook/react'
 import { Providers } from '../providers'
 import { DotMenu, DotMenuItem } from '../../../src'
+
+const useStyles = makeStyles((theme) => {
+  const { colors } = theme.palette
+
+  return {
+    container: {
+      backgroundColor: colors.darkerBlue,
+      width: theme.spacing(20),
+    },
+    button: {
+      color: colors.white,
+    },
+  }
+})
+
+const WhiteDotMenuOnBlueBackground = () => {
+  const classes = useStyles()
+
+  return (
+    <div className={classes.container}>
+      <DotMenu
+        className={classes.button}
+        classes={{
+          button: classes.button,
+        }}
+      >
+        <DotMenuItem onClick={() => console.log('item one clicked')}>Item one</DotMenuItem>
+        <DotMenuItem onClick={() => console.log('item two clicked')}>Item two</DotMenuItem>
+      </DotMenu>
+    </div>
+  )
+}
 
 const stories = () => {
   storiesOf('Dot Menu', module)
@@ -28,10 +60,17 @@ const stories = () => {
                 <DotMenuItem onClick={() => console.log('item two clicked')}>Item two</DotMenuItem>
               </DotMenu>
             </Grid>
+            <Grid item>
+              <Typography variant="h3" align="left">
+                White dot menu on blue background
+              </Typography>
+              <WhiteDotMenuOnBlueBackground/>
+            </Grid>
           </Grid>
         </div>
       </Providers>
     ))
 }
+
 
 export default stories
