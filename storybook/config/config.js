449ed7ab044162dc37a8bcde331902c6c2f67d49
Christoph Vitz <c.vitz@itsonix.eu>
2019-08-16 11:06:12 +0200

    PA-1230 [REVIEW]: Use all stories by default

--- storybook/config/config.js
+++ storybook/config/config.js
@@ -1,21 +1,6 @@
 import { addParameters, configure } from '@storybook/react'
 
-import {
-  avatarStories,
-  buttonStories,
-  colorStories,
-  dotMenuStories,
-  errorBoxStories,
-  iconButtonStories,
-  inputStories,
-  selectStories,
-  linkStories,
-  typographyStories,
-  headerBarStories,
-  modalDialogStories,
-  logoPageStories,
-  iconStories,
-} from '../stories/index'
+import * as stories from '../stories/index'
 
 addParameters({
   options: {
@@ -24,20 +9,9 @@ addParameters({
 })
 
 function loadStories() {
-  avatarStories()
-  colorStories()
-  dotMenuStories()
-  buttonStories()
-  iconButtonStories()
-  linkStories()
-  typographyStories()
-  inputStories()
-  selectStories()
-  errorBoxStories()
-  headerBarStories()
-  modalDialogStories()
-  logoPageStories()
-  iconStories()
+  for (const story of Object.values(stories)) {
+    story()
+  }
 }
 
 configure(loadStories, module)
