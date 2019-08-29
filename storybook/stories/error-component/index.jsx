e073e0bba0ee42faf8444b24b4b4ac0c8f1bdf76
Wasiq Kashkari <w.kashkari@outlook.com>
2019-08-29 16:56:42 +1200

    PA-1287: add story for generic error component

---
+++ storybook/stories/error-component/index.jsx
@@ -0,0 +1,26 @@
+
+import React from 'react'
+import { storiesOf } from '@storybook/react'
+import { Typography } from '@material-ui/core'
+
+import { ErrorComponent } from '../../../src'
+import { Providers } from '../providers'
+
+const errorBoxStories = () => {
+  storiesOf('Error Component', module)
+    .add('Error Component', () => (
+      <Providers>
+        <Typography variant="h2" align="left">
+            Error Component
+        </Typography>
+        <ErrorComponent
+          title="My Error"
+          description="My error description is here"
+        >
+          <div> My error children are here </div>
+        </ErrorComponent>
+      </Providers>
+    ))
+}
+
+export default errorBoxStories
