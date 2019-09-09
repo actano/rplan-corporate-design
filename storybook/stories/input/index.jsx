e5586c95e242ab4e48768f3088b7f4ba64af3326
Christoph Vitz <c.vitz@itsonix.eu>
2019-09-09 11:17:38 +0200

    A-1481: Extract story to file

--- storybook/stories/input/index.jsx
+++ storybook/stories/input/index.jsx
@@ -1,87 +1,9 @@
-import React from 'react'
-import { Grid, TextField, Typography } from '@material-ui/core'
 import { storiesOf } from '@storybook/react'
-import { Providers } from '../providers'
+import { defaultInputStory } from './default-inputs'
 
 const inputStories = () => {
   storiesOf('Input', module)
-    .add('Input', () =>
-      (
-        <Providers>
-          <div style={{ paddingLeft: '20px' }}>
-            <Grid
-              container
-              spacing={2}
-              direction="column"
-            >
-              <Grid item>
-                <Typography variant="h2" align="left">
-                  Input
-                </Typography>
-              </Grid>
-              <form>
-                <Grid item>
-                  <Typography variant="h3" align="left">
-                    Normal Input
-                  </Typography>
-                  <TextField
-                    margin="normal"
-                    variant="outlined"
-                    label="Last name"
-                    InputLabelProps={{
-                      shrink: true,
-                    }}
-                  />
-                </Grid>
-                <Grid item>
-                  <Typography variant="h3" align="left">
-                    Normal Input with placeholder
-                  </Typography>
-                  <TextField
-                    margin="normal"
-                    variant="outlined"
-                    label="Last name"
-                    placeholder="Last name"
-                    InputLabelProps={{
-                      shrink: true,
-                    }}
-                  />
-                </Grid>
-                <Grid item>
-                  <Typography variant="h3" align="left">
-                    Error in input
-                  </Typography>
-                  <TextField
-                    margin="normal"
-                    variant="outlined"
-                    label="Enter: 'abc'"
-                    value="cde"
-                    error
-                    InputLabelProps={{
-                      shrink: true,
-                    }}
-                  />
-                </Grid>
-                <Grid item>
-                  <Typography variant="h3" align="left">
-                    Disabled Input
-                  </Typography>
-                  <TextField
-                    margin="normal"
-                    variant="outlined"
-                    label="disabled"
-                    value="disabled"
-                    disabled
-                    InputLabelProps={{
-                      shrink: true,
-                    }}
-                  />
-                </Grid>
-              </form>
-            </Grid>
-          </div>
-        </Providers>
-      ))
+    .add('Default Inputs', defaultInputStory)
 }
 
 export default inputStories
