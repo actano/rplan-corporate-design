78915d01fb5553a161299e41372cd42f2d88bb25
Christoph Vitz <c.vitz@itsonix.eu>
2019-09-09 10:59:16 +0200

    PA-1481: Add `DialogBoxInput` component

---
+++ src/components/dialog-box-input.jsx
@@ -0,0 +1,55 @@
+import InputBase from '@material-ui/core/InputBase'
+import { makeStyles } from '@material-ui/styles'
+import React from 'react'
+
+const useStyles = makeStyles((theme) => {
+  const { colors } = theme.palette
+
+  return {
+    input: {
+      fontSize: '0.875rem',
+      textOverflow: 'ellipsis',
+      lineHeight: '1.43',
+      padding: theme.spacing(1.75, 2.125),
+
+      color: colors.darkestGrey,
+
+      '&::placeholder': {
+        color: colors.grey,
+      },
+    },
+    inputRoot: {
+      border: `1px solid ${colors.lightGrey}`,
+
+      '&:focus-within': {
+        color: colors.grey,
+        borderColor: colors.lightGrey,
+      },
+
+      '&:hover': {
+        color: colors.grey,
+        borderColor: colors.grey,
+      },
+    },
+  }
+})
+
+const DialogBoxInput = React.forwardRef((
+  { ...otherProps },
+  ref,
+) => {
+  const classes = useStyles()
+
+  return (
+    <InputBase
+      ref={ref}
+      {...otherProps}
+      classes={{
+        root: classes.inputRoot,
+        input: classes.input,
+      }}
+    />
+  )
+})
+
+export { DialogBoxInput }
