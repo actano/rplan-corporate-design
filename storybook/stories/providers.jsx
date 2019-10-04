e6d55771a244bac5e80e3dc3e3ed4aa66a96c6ee
Christoph Vitz <c.vitz@itsonix.eu>
2019-10-04 10:00:37 +0200

    PA-1932: Add Provider for date pickers

--- storybook/stories/providers.jsx
+++ storybook/stories/providers.jsx
@@ -1,4 +1,6 @@
 import { CssBaseline } from '@material-ui/core'
+import { MuiPickersUtilsProvider } from '@material-ui/pickers'
+import DateFnsUtils from '@date-io/date-fns'
 import { ThemeProvider } from '@material-ui/styles'
 import React from 'react'
 import createMuiTheme from '@material-ui/core/styles/createMuiTheme'
@@ -10,8 +12,10 @@ const theme = createMuiTheme(themeConfig)
 // eslint-disable-next-line react/prop-types
 const Providers = ({ children }) => (
   <ThemeProvider theme={theme}>
-    <CssBaseline />
-    { children }
+    <MuiPickersUtilsProvider utils={DateFnsUtils}>
+      <CssBaseline />
+      { children }
+    </MuiPickersUtilsProvider>
   </ThemeProvider>
 )
 
