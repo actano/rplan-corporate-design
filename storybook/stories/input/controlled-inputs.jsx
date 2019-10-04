9a2e1d79dc0d1d832f4209a470cd0df9ede47664
Christoph Vitz <c.vitz@itsonix.eu>
2019-10-04 14:38:52 +0200

    PA-1932: Add storybook entry for usage of HoC

--- storybook/stories/input/controlled-inputs.jsx
+++ storybook/stories/input/controlled-inputs.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
 import { actions } from '@storybook/addon-actions'
-import { Grid, Typography } from '@material-ui/core'
+import {
+  Grid, TextField, Typography, InputAdornment,
+} from '@material-ui/core'
+import { withBlurOnEnter, withSaveOnBlurControlled } from '../../../src/inputs'
 import { DefaultControlledInput } from '../../../src/inputs/default-controlled-input'
 import { DefaultControlledTextField } from '../../../src/inputs/default-controlled-text-field'
 import { Providers } from '../providers'
+
+const CustomInput = props => (
+  <TextField
+    InputProps={{
+      endAdornment: <InputAdornment position="start">Kg</InputAdornment>,
+    }}
+    {...props}
+  />
+)
+
+const CustomControlledInput = withSaveOnBlurControlled(
+  withBlurOnEnter(
+    CustomInput,
+  ),
+)
 
 const onSaveHandler = actions('onSave')
 
@@ -39,6 +57,22 @@ const controlledInputStory = () =>
           <DefaultControlledTextField
             placeholder="write some text"
             originalValue="initial text"
+            {...onSaveHandler}
+          />
+        </Grid>
+
+        <Grid item>
+          <Typography variant="h2">
+            Custom Controlled Input
+          </Typography>
+          <Typography variant="body1">
+            This input uses two HoC which create an input, that handles its value as
+            internal state and provides changes via event-handlers.
+            Also pressing enter blurs the input.
+            The actual input component can be any material ui input component.
+          </Typography>
+          <CustomControlledInput
+            originalValue="42"
             {...onSaveHandler}
           />
         </Grid>
