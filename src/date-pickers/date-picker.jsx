0a8726d087b1c99b8c636812adb640f009e827a4
thrucker <t.rucker@xpure.de>
2019-10-30 09:40:13 +0100

    PA-2312: Add testable date picker component
    This wraps the original date picker components and adds data attributes
    (where possible) in order to be able to functionally test the date
    picker component.

---
+++ src/date-pickers/date-picker.jsx
@@ -0,0 +1,60 @@
+import React, { useCallback, useContext } from 'react'
+import PropTypes from 'prop-types'
+import { DatePicker as DatePickerMui, MuiPickersContext } from '@material-ui/pickers'
+import { testIdProp } from '@rplan/testhelpers-webclient'
+
+const DatePicker = ({
+  leftArrowButtonProps,
+  rightArrowButtonProps,
+  renderDay,
+  ...props
+}) => {
+  const dateUtils = useContext(MuiPickersContext)
+
+  const _renderDay = useCallback(
+    (day, selectedDate, dayInCurrentMonth, dayComponent) => {
+      const elementWithData = React.cloneElement(
+        dayComponent,
+        { 'data-date': dateUtils.format(day, 'yyyy-MM-dd') },
+      )
+
+      if (renderDay != null) {
+        return renderDay(day, selectedDate, dayInCurrentMonth, elementWithData)
+      }
+
+      return elementWithData
+    },
+    [dateUtils, renderDay],
+  )
+
+  return (
+    <DatePickerMui
+      leftArrowButtonProps={{
+        ...leftArrowButtonProps,
+        ...testIdProp('date-picker-left-arrow'),
+      }}
+      rightArrowButtonProps={{
+        ...rightArrowButtonProps,
+        ...testIdProp('date-picker-right-arrow'),
+      }}
+      renderDay={_renderDay}
+      {...props}
+    />
+  )
+}
+
+DatePicker.propTypes = {
+  leftArrowButtonProps: PropTypes.objectOf(PropTypes.string),
+  rightArrowButtonProps: PropTypes.objectOf(PropTypes.string),
+  renderDay: PropTypes.func,
+}
+
+DatePicker.defaultProps = {
+  leftArrowButtonProps: {},
+  rightArrowButtonProps: {},
+  renderDay: undefined,
+}
+
+export {
+  DatePicker,
+}
