58357f2e74f61289cd0af77c18ac831565b7c6e8
Wasiq Kashkari <w.kashkari@outlook.com>
2020-01-20 13:18:45 +0100

    PA-3000: Add rich-text-editor to corporate design

---
+++ src/inputs/rich-text-input.jsx
@@ -0,0 +1,297 @@
+import PropTypes from 'prop-types'
+import React, { useState, useCallback } from 'react'
+import { makeStyles } from '@material-ui/core'
+import CKEditor from '@ckeditor/ckeditor5-react'
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic'
+import cx from 'classnames'
+
+import { PrimaryButton, SecondaryButton } from '../buttons'
+
+const createHeaderStyles = (classNamePrefix, theme) => console.log(theme) || ({
+  [`& ${classNamePrefix}1`]: {
+    fontSize: '0.9375rem',
+    lineHeight: '1.5rem',
+    fontWeight: 600,
+    color: theme.palette.colors.black,
+  },
+  [`& ${classNamePrefix}2`]: {
+    fontSize: '0.875rem',
+    lineHeight: '1.5rem',
+    fontWeight: 600,
+    color: theme.palette.colors.black,
+  },
+  [`& ${classNamePrefix}3`]: {
+    fontSize: '0.8125rem',
+    lineHeight: '1.25rem',
+    fontWeight: 600,
+    color: theme.palette.colors.black,
+  },
+})
+
+const createLinkStyles = theme => ({
+  '& a': {
+    color: theme.palette.colors.blue,
+    '&:visited': {
+      color: theme.palette.colors.darkBlue,
+    },
+  },
+})
+
+const createBlockquoteStyles = theme => ({
+  '& blockquote': {
+    overflow: 'hidden',
+    paddingRight: '1.5em',
+    paddingLeft: '1.5em',
+    marginLeft: 0,
+    marginRight: 0,
+    fontStyle: 'italic',
+    borderLeft: `5px solid ${theme.palette.colors.veryLightGrey}`,
+  },
+
+})
+
+const useStyles = makeStyles(theme => ({
+  details: {
+    outline: 'none',
+    ...createBlockquoteStyles(theme),
+    ...createHeaderStyles('h', theme),
+    ...createLinkStyles(theme),
+  },
+  descriptionInput: {
+    lineHeight: '1.54',
+    fontSize: '0.8125rem',
+    color: theme.palette.colors.darkestGrey,
+    '&::placeholder': {
+      color: theme.palette.colors.grey,
+    },
+  },
+  placeholder: {
+    lineHeight: '1.54',
+    fontSize: '0.8125rem',
+    color: theme.palette.colors.grey,
+  },
+  buttonContainer: {
+    marginTop: theme.spacing(2),
+    display: 'flex',
+    justifyContent: 'flex-end',
+  },
+  saveButton: {
+    fontSize: '0.75rem',
+    padding: '4px 12px',
+  },
+  cancelButton: {
+    fontSize: '0.75rem',
+    padding: '4px 12px',
+    marginRight: theme.spacing(1),
+  },
+  // This nesting is to
+  '@global': {
+    ':root': {
+      // ---------- tooltip
+      '--ck-color-tooltip-text': theme.palette.colors.white,
+      // @TODO fix this color
+      '--ck-color-tooltip-background': theme.palette.colors.darkGrey,
+
+      '--ck-color-base-background': theme.palette.colors.white,
+      '--ck-color-base-border': theme.palette.colors.veryLightGrey,
+
+      // --------- toolbar
+      '--ck-color-text': theme.palette.colors.darkGrey,
+      '--ck-color-base-foreground': `${theme.palette.colors.lightestGrey}80`,
+
+      // --------- toolbar buttons
+      '--ck-color-button-default-background': 'transparent',
+
+      '--ck-color-button-default-hover-background': theme.palette.colors.veryLightGrey,
+      '--ck-color-button-on-hover-background': theme.palette.colors.veryLightGrey,
+      '--ck-color-button-action-hover-background': theme.palette.colors.veryLightGrey,
+
+      '--ck-color-button-default-disabled-background': 'transparent',
+      '--ck-color-button-on-disabled-background': 'transparent',
+      '--ck-color-button-action-disabled-background': 'transparent',
+
+      '--ck-color-button-default-active-background': theme.palette.colors.veryLightGrey,
+      '--ck-color-button-on-active-background': theme.palette.colors.veryLightGrey,
+      '--ck-color-button-action-active-background': theme.palette.colors.veryLightGrey,
+
+      '--ck-color-button-default-active-shadow': theme.palette.colors.lightGrey,
+      '--ck-color-button-action-active-shadow': theme.palette.colors.lightGrey,
+      '--ck-color-button-on-active-shadow': theme.palette.colors.lightGrey,
+
+      '--ck-color-button-on-background': theme.palette.colors.veryLightGrey,
+      '--ck-color-button-action-background': theme.palette.colors.veryLightGrey,
+
+      '--ck-color-list-button-on-text': theme.palette.colors.darkGrey,
+      '--ck-color-list-button-on-background': theme.palette.colors.lightGrey,
+      '--ck-color-list-button-on-background-focus': theme.palette.colors.lightGrey,
+      '--ck-color-list-button-hover-background': theme.palette.colors.veryLightGrey,
+
+      '--ck-border-radius': 2,
+      '--ck-color-focus-border': theme.palette.colors.blue,
+      '--ck-focus-outer-shadow': '0px 0px',
+
+      '--ck-font-size-base': '0.8125rem',
+      '--ck-line-height-base': 1.54,
+      '--ck-font-face': theme.typography.fontFamily,
+
+      // taken from material-ui to make the dropdown box shadows consistent with others
+      '--ck-drop-shadow': '0px 5px 5px -3px rgba(0,0,0,0.2), 0px 8px 10px 1px rgba(0,0,0,0.14), 0px 3px 14px 2px rgba(0,0,0,0.12)',
+      '--ck-color-button-save': theme.palette.colors.green,
+      '--ck-color-button-cancel': theme.palette.colors.red,
+    },
+    '.ck': {
+      color: theme.palette.colors.darkestGrey,
+      fontSize: '0.8125rem',
+      '& .ck-placeholder': {
+        color: theme.palette.colors.grey,
+      },
+      '& .ck-dropdown__panel': {
+        // there is no override for border
+        border: 'none',
+      },
+      '& .ck-list': {
+        // there is no override for the margins within a list
+        marginTop: theme.spacing(1),
+        marginBottom: theme.spacing(1),
+      },
+      '& h1, h2, h3': {
+        fontWeight: 600,
+      },
+      ...createBlockquoteStyles(theme),
+      ...createLinkStyles(theme),
+      ...createHeaderStyles('.ck-heading_heading', theme),
+      ...createHeaderStyles('h', theme),
+    },
+  },
+}))
+
+const placeholder = 'Add a description'
+
+const baseEditorConfig = {
+  copyFormatting_allowedContexts: true,
+  heading: {
+    options: [
+      {
+        model: 'paragraph',
+        title: 'Paragraph',
+        class: 'ck-heading_paragraph',
+      }, {
+        model: 'heading1',
+        view: 'h1',
+        title: 'Heading 1',
+        class: 'ck-heading_heading1',
+      },
+      {
+        model: 'heading2',
+        view: 'h2',
+        title: 'Heading 2',
+        class: 'ck-heading_heading2',
+      },
+      {
+        model: 'heading3',
+        view: 'h3',
+        title: 'Heading 3',
+        class: 'ck-heading_heading3',
+      },
+    ],
+  },
+  removePlugins: [
+    'Image',
+    'ImageCaption',
+    'ImageStyle',
+    'ImageToolbar',
+    'ImageUpload',
+    'CKFinderUploadAdapter',
+    'CKFinder',
+    'EasyImage',
+  ],
+  toolbar: [
+    'heading', '|',
+    'bold', 'italic', '|',
+    'link', 'bulletedList', 'numberedList', 'indent', 'outdent', '|',
+    'blockQuote', '|',
+    'undo', 'redo',
+  ],
+  link: {
+    addTargetToExternalLinks: true,
+  },
+}
+
+export const RichTextInput = ({
+  onSave, originalValue, testId, className, placeholder,
+}) => {
+  const classes = useStyles()
+  const [isEditorOpen, setIsEditorOpen] = useState(false)
+  const [data, setData] = useState(originalValue)
+  const editorConfig = { ...baseEditorConfig, placeholder }
+
+  const onChangeEditorData = useCallback((event, editor) => {
+    setData(editor.getData())
+  }, [])
+  const onSaveClick = useCallback(() => {
+    onSave(data)
+    setIsEditorOpen(false)
+  }, [data, onSave])
+  const onCancelClick = useCallback(() => {
+    setIsEditorOpen(false)
+  }, [])
+
+  if (isEditorOpen) {
+    return (
+      <div classes={cx(classes.details, className)}>
+        <CKEditor
+          editor={ClassicEditor}
+          data={originalValue}
+          onChange={onChangeEditorData}
+          config={editorConfig}
+        />
+        <div className={classes.buttonContainer}>
+          <SecondaryButton className={classes.cancelButton} onClick={onCancelClick}>
+              Cancel
+          </SecondaryButton>
+          <PrimaryButton className={classes.saveButton} onClick={onSaveClick}>
+              Save
+          </PrimaryButton>
+        </div>
+      </div>
+    )
+  }
+  return (
+    <div
+      role="button"
+      tabIndex={0}
+      className={classes.details}
+      onKeyPress={() => setIsEditorOpen(true)}
+      onClick={() => setIsEditorOpen(true)}
+      data-test-id={testId}
+    >
+      {
+          originalValue
+            ? (
+              <div
+                className={classes.descriptionInput}
+                // eslint-disable-next-line react/no-danger
+                dangerouslySetInnerHTML={{ __html: originalValue }}
+              />
+            )
+            : (<div className={classes.placeholder}><p>{placeholder}</p></div>)
+        }
+    </div>
+  )
+}
+
+RichTextInput.propTypes = {
+  originalValue: PropTypes.string,
+  onSave: PropTypes.func.isRequired,
+  testId: PropTypes.string,
+  placeholder: PropTypes.string,
+  className: PropTypes.string,
+}
+
+
+RichTextInput.defaultProps = {
+  originalValue: '',
+  testId: '',
+  className: '',
+  placeholder: 'Add some text',
+}
--- storybook/stories/input/specific-inputs.jsx
+++ storybook/stories/input/specific-inputs.jsx
@@ -4,6 +4,7 @@ import { Grid, Typography } from '@material-ui/core'
 import { DefaultControlledTextField } from '../../../src/inputs/default-controlled-text-field'
 import { InputWithLabel } from '../../../src/inputs/input-with-label'
 import { NumberInput } from '../../../src/inputs/number-input'
+import { RichTextInput } from '../../../src/inputs/rich-text-input'
 import { Providers } from '../providers'
 
 const onSaveHandler = actions('onSave')
@@ -43,6 +44,19 @@ const specificInputStory = () =>
           <NumberInput
             originalValue={10}
             getValidationError={number => (number < 42 ? 'Number must be above 42' : null)}
+            {...onSaveHandler}
+          />
+        </Grid>
+        <Grid item>
+          <Typography variant="h2">
+            Rich Text Input
+          </Typography>
+          <Typography variant="body1">
+            This input allows the input of rich text.
+            It is an uncontrolled input and only calls the onSave handler if the user clicks save.
+          </Typography>
+          <RichTextInput
+            originalValue={'<h1> Hello There </h1>'}
             {...onSaveHandler}
           />
         </Grid>
