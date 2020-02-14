f172c210f303993c9a27943c55cfb76fb0edda0a
DagmarTimmreck <dagmar.timmreck@actano.de>
2020-02-14 11:10:17 +0100

    PA-3308: Configure storybook to use knobs

--- storybook/config/config.js
+++ storybook/config/config.js
@@ -1,5 +1,5 @@
-import { addParameters, configure } from '@storybook/react'
-
+import { addDecorator, addParameters, configure } from '@storybook/react'
+import { withKnobs } from '@storybook/addon-knobs'
 import * as stories from '../stories/index'
 
 addParameters({
@@ -8,6 +8,8 @@ addParameters({
     panelPosition: 'bottom',
   },
 })
+
+addDecorator(withKnobs)
 
 function loadStories() {
   for (const story of Object.values(stories)) {
