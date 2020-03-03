a409aebd6d8f12addf65cc3ef4942e5a4485c606
Christoph Vitz <c.vitz@itsonix.eu>
2020-03-03 09:54:14 +0100

    PA-3630: Extend user avatar storybook with all permutations
    We had some bugs in some of the possible permutations (currently 2^3)
    
    with/without user
    with/without gravatar
    enabled/disabled

--- storybook/stories/avatar/index.jsx
+++ storybook/stories/avatar/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Grid, Typography, makeStyles } from '@material-ui/core'
+import { Grid, Typography } from '@material-ui/core'
 import { actions } from '@storybook/addon-actions'
 import { boolean } from '@storybook/addon-knobs'
 import { storiesOf } from '@storybook/react'
@@ -10,31 +10,32 @@ import { Providers } from '../providers'
 
 const onClickAction = actions('onClick')
 
-const useStyles = makeStyles(theme => ({
-  withPlaceholder: {
-    color: theme.palette.colors.grey,
-    borderColor: theme.palette.colors.lightGrey,
-  },
-}))
-
 // eslint-disable-next-line react/prop-types
-const UserAvatarWithPlaceholderWithStyles = ({ size, disabled }) => {
-  const classes = useStyles()
-  return (
-    <UserAvatarWithPlaceholder
-      classes={{ color: classes.withPlaceholder }}
-      user={null}
-      size={size}
-      disabled={disabled}
-      {...onClickAction}
-    />
-  )
-}
+const UserAvatarWithPlaceholderWithDefaults = ({ size, user, disabled }) => (
+  <UserAvatarWithPlaceholder
+    user={user}
+    size={size}
+    disabled={disabled}
+    {...onClickAction}
+  />
+)
+
 
 const avatarStories = () => {
   storiesOf('User Avatar', module)
     .add('UserAvatar', () => {
-      const disabledKnob = boolean('disable UserAvatarWithPlaceholder', false)
+      const disabledKnob = boolean('UserAvatarWithPlaceholder: disabled', false)
+      const hasUser = boolean('UserAvatarWithPlaceholder: hasUser', true)
+      const userHasGravatarEmail = boolean('Example user: has gravtar image', true)
+
+      const sampleUser = {
+        firstName: 'Example',
+        lastName: 'Smith',
+        email: userHasGravatarEmail ? 'marie.omann@actano.de' : 'invalidEmail@example.com',
+      }
+
+      const user = hasUser ? sampleUser : null
+
       return (
         <Providers>
           <div style={{ paddingLeft: '20px' }}>
@@ -53,10 +54,8 @@ const avatarStories = () => {
                 <div style={{ display: 'flex', alignItems: 'center', marginTop: '20px' }}>
                   <div>
                     <UserAvatar
-                      email="marie.omann@actano.de"
-                      firstName="Example"
-                      lastName="Smith"
                       size="regular"
+                      {...sampleUser}
                     />
                     <Typography variant="h5" align="left">
                       regular
@@ -64,9 +63,7 @@ const avatarStories = () => {
                   </div>
                   <div style={{ marginLeft: '20px' }}>
                     <UserAvatar
-                      email="marie.omann@actano.de"
-                      firstName="Example"
-                      lastName="Smith"
+                      {...sampleUser}
                       size="small-2"
                     />
                     <Typography variant="h5" align="left">
@@ -75,49 +72,7 @@ const avatarStories = () => {
                   </div>
                   <div style={{ marginLeft: '20px' }}>
                     <UserAvatar
-                      email="marie.omann@actano.de"
-                      firstName="Example"
-                      lastName="Smith"
-                      size="small"
-                    />
-                    <Typography variant="h5" align="left">
-                      small
-                    </Typography>
-                  </div>
-                </div>
-              </Grid>
-              <Grid item>
-                <Typography variant="h3" align="left">
-                  User Avatar with initials (when the gravatar picture is not found)
-                </Typography>
-                <div style={{ display: 'flex', alignItems: 'center', marginTop: '20px' }}>
-                  <div>
-                    <UserAvatar
-                      email="invalidEmail@example.com"
-                      firstName="Example"
-                      lastName="Smith"
-                      size="regular"
-                    />
-                    <Typography variant="h5" align="left">
-                      regular
-                    </Typography>
-                  </div>
-                  <div style={{ marginLeft: '20px' }}>
-                    <UserAvatar
-                      email="invalidEmail@example.com"
-                      firstName="Example"
-                      lastName="Smith"
-                      size="small-2"
-                    />
-                    <Typography variant="h5" align="left">
-                      small-2
-                    </Typography>
-                  </div>
-                  <div style={{ marginLeft: '20px' }}>
-                    <UserAvatar
-                      email="invalidEmail@example.com"
-                      firstName="Example"
-                      lastName="Smith"
+                      {...sampleUser}
                       size="small"
                     />
                     <Typography variant="h5" align="left">
@@ -132,19 +87,19 @@ const avatarStories = () => {
                 </Typography>
                 <div style={{ display: 'flex', alignItems: 'center', marginTop: '20px' }}>
                   <div>
-                    <UserAvatarWithPlaceholderWithStyles size="regular" disabled={disabledKnob} />
+                    <UserAvatarWithPlaceholderWithDefaults user={user} size="regular" disabled={disabledKnob} />
                     <Typography variant="h5" align="left">
                       regular
                     </Typography>
                   </div>
                   <div style={{ marginLeft: '20px' }}>
-                    <UserAvatarWithPlaceholderWithStyles size="small-2" disabled={disabledKnob} />
+                    <UserAvatarWithPlaceholderWithDefaults user={user} size="small-2" disabled={disabledKnob} />
                     <Typography variant="h5" align="left">
                       small-2
                     </Typography>
                   </div>
                   <div style={{ marginLeft: '20px' }}>
-                    <UserAvatarWithPlaceholderWithStyles size="small" disabled={disabledKnob} />
+                    <UserAvatarWithPlaceholderWithDefaults user={user} size="small" disabled={disabledKnob} />
                     <Typography variant="h5" align="left">
                       small
                     </Typography>
