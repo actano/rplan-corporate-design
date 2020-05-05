2c94fa8652d641379fca0fa675d32c79f140687e
Renato Gama <renatoargh@gmail.com>
2020-05-05 11:07:25 +0200

    PA-4720: Add i18n publish script and add version to useTranslation wrapper

--- src/i18n.jsx
+++ src/i18n.jsx
@@ -1,3 +1,5 @@
 import { useTranslation as originalUseTranslation } from 'react-i18next'
+import pkg from '../package.json'
 
-export const useTranslation = () => originalUseTranslation('corporate-design:translations')
+export const useTranslation = (fileName = 'translations') =>
+  originalUseTranslation(`corporate-design:${pkg.version}:${fileName}`)
