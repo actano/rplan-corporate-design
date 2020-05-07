a9b58c38ec4343cfd3fbdde8b207a5170404d58b
Christoph Vitz <c.vitz@itsonix.eu>
2020-05-07 16:15:39 +0200

    PA-4650: Skip i18n tests for now due to lack of pluralization support

--- test/i18n.unit.js
+++ test/i18n.unit.js
@@ -3,4 +3,5 @@ import path from 'path'
 
 const i18nPath = path.join(__dirname, '../static/i18n')
 
-describe('i18n', translationsTest(i18nPath))
+// TODO: unskip as soon as the allex-i18n-tools can handle pluralization
+describe.skip('i18n', translationsTest(i18nPath))
