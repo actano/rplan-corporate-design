ac1735765343adf388faa1161da62963865143b6
Christoph Vitz <c.vitz@itsonix.eu>
2020-05-08 07:58:46 +0200

    PA-4650: Upgrade i18n-test-tools and unskip test again

--- test/i18n.unit.js
+++ test/i18n.unit.js
@@ -3,5 +3,4 @@ import path from 'path'
 
 const i18nPath = path.join(__dirname, '../static/i18n')
 
-// TODO: unskip as soon as the allex-i18n-tools can handle pluralization
-describe.skip('i18n', translationsTest(i18nPath))
+describe('i18n', translationsTest(i18nPath))
