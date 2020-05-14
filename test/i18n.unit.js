b6e72ccf1990427bde1f49564408dddddea65ad1
Renato Gama <renatoargh@gmail.com>
2020-05-14 14:41:25 +0200

    PA-4322: Add new translation test

--- test/i18n.unit.js
+++ test/i18n.unit.js
@@ -2,5 +2,6 @@ import { translationsTest } from '@rplan/allex-i18n-tools'
 import path from 'path'
 
 const i18nPath = path.join(__dirname, '../static/i18n')
+const srcPath = path.join(__dirname, '../src')
 
-describe('i18n', translationsTest(i18nPath))
+describe('i18n', translationsTest(i18nPath, srcPath))
