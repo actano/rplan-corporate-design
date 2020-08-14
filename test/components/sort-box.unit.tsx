82d9e7ff3cc2a343b688279f17f484f820d3384b
Nikita Zotsik <nikita.exceed@gmail.com>
2020-08-14 14:00:14 +0300

    PA-6064 extend test for SortBox

--- test/components/sort-box.unit.tsx
+++ test/components/sort-box.unit.tsx
@@ -10,135 +10,100 @@ import { settleComponent } from '@rplan/testhelpers-webclient'
 
 import { themeConfig } from '../../src/theme/theme-config'
 import {
-  SortField, SortFieldType, SortBox, SortBoxProps,
+  SortField, SortFieldType, SortBox, SortBoxProps, SortOrder,
 } from '../../src/components/sort-box'
 import { testIdProp } from '../../src/shared/test-ids'
 
 const delay = t => new Promise(resolve => setTimeout(resolve, t))
 
 interface Task {
-  name: string,
-  startDate: LocalDate,
+  name: string
+  startDate: LocalDate
 }
 
 interface TestDataType {
-  id: string,
-  task: Task,
-  responsible: string,
+  id: string
+  task: Task
+  responsible: string
+}
+
+const testItem1 = {
+  id: 'aaa1',
+  task: {
+    name: 'Revert to last version',
+    startDate: LocalDate.parse('2020-07-24'),
+  },
+  responsible: 'Zenia K. Owen',
+}
+
+const testItem2 = {
+  id: 'aaa2',
+  task: {
+    name: 'Do some noise',
+    startDate: LocalDate.parse('2020-07-23'),
+  },
+  responsible: 'Zenia K. Owen',
+}
+
+const testItem3 = {
+  id: 'aaa3',
+  task: {
+    name: 'Build project',
+    startDate: LocalDate.parse('2020-07-25'),
+  },
+  responsible: 'Anika I. Emerson',
+}
+
+const testItem4 = {
+  id: 'aaa4',
+  task: {
+    name: 'Restore DB dump',
+    startDate: LocalDate.parse('2020-07-20'),
+  },
+  responsible: 'Neville H. Hess',
 }
 
 const testList: TestDataType[] = [
-  {
-    id: 'aaa1',
-    task: {
-      name: 'Revert to last version',
-      startDate: LocalDate.parse('2020-07-24'),
-    },
-    responsible: 'Zenia K. Owen',
-  },
-  {
-    id: 'aaa2',
-    task: {
-      name: 'Do some noise',
-      startDate: LocalDate.parse('2020-07-23'),
-    },
-    responsible: 'Zenia K. Owen',
-  },
-  {
-    id: 'aaa3',
-    task: {
-      name: 'Build project',
-      startDate: LocalDate.parse('2020-07-25'),
-    },
-    responsible: 'Anika I. Emerson',
-  },
-  {
-    id: 'aaa4',
-    task: {
-      name: 'Restore DB dump',
-      startDate: LocalDate.parse('2020-07-20'),
-    },
-    responsible: 'Neville H. Hess',
-  },
+  testItem1,
+  testItem2,
+  testItem3,
+  testItem4,
 ]
 
-const testListSortedByTaskName: TestDataType[] = [
-  {
-    id: 'aaa3',
-    task: {
-      name: 'Build project',
-      startDate: LocalDate.parse('2020-07-25'),
-    },
-    responsible: 'Anika I. Emerson',
-  },
-  {
-    id: 'aaa2',
-    task: {
-      name: 'Do some noise',
-      startDate: LocalDate.parse('2020-07-23'),
-    },
-    responsible: 'Zenia K. Owen',
-  },
-  {
-    id: 'aaa4',
-    task: {
-      name: 'Restore DB dump',
-      startDate: LocalDate.parse('2020-07-20'),
-    },
-    responsible: 'Neville H. Hess',
-  },
-  {
-    id: 'aaa1',
-    task: {
-      name: 'Revert to last version',
-      startDate: LocalDate.parse('2020-07-24'),
-    },
-    responsible: 'Zenia K. Owen',
-  },
+const testListSortedByTaskNameAsc: TestDataType[] = [
+  testItem3,
+  testItem2,
+  testItem4,
+  testItem1,
 ]
 
-const testListSortedByTaskStartDate: TestDataType[] = [
-  {
-    id: 'aaa1',
-    task: {
-      name: 'Revert to last version',
-      startDate: LocalDate.parse('2020-07-24'),
-    },
-    responsible: 'Zenia K. Owen',
-  },
-  {
-    id: 'aaa2',
-    task: {
-      name: 'Do some noise',
-      startDate: LocalDate.parse('2020-07-23'),
-    },
-    responsible: 'Zenia K. Owen',
-  },
-  {
-    id: 'aaa3',
-    task: {
-      name: 'Build project',
-      startDate: LocalDate.parse('2020-07-25'),
-    },
-    responsible: 'Anika I. Emerson',
-  },
-  {
-    id: 'aaa4',
-    task: {
-      name: 'Restore DB dump',
-      startDate: LocalDate.parse('2020-07-20'),
-    },
-    responsible: 'Neville H. Hess',
-  },
+const testListSortedByTaskStartDateAsc: TestDataType[] = [
+  testItem1,
+  testItem2,
+  testItem3,
+  testItem4,
 ]
 
+const testListSortedByTaskNameDesc: TestDataType[] = [
+  testItem1,
+  testItem4,
+  testItem2,
+  testItem3,
+]
+
+const testListSortedByTaskStartDateDesc: TestDataType[] = [
+  testItem3,
+  testItem1,
+  testItem2,
+  testItem4,
+]
 
 const defaultSort: SortField = {
   sortName: 'default',
   fieldType: SortFieldType.DEFAULT,
 }
 
-const sortFields: SortField[] = [
+const sortFieldsAsc: SortField[] = [
   defaultSort,
   {
     sortName: 'task name',
@@ -157,6 +122,28 @@ const sortFields: SortField[] = [
   },
 ]
 
+const sortFieldsDesc: SortField[] = [
+  defaultSort,
+  {
+    sortName: 'task name',
+    fieldType: SortFieldType.STRING,
+    sortFieldPath: ['task', 'name'],
+    order: SortOrder.DESC,
+  },
+  {
+    sortName: 'start date',
+    fieldType: SortFieldType.LOCAL_DATE,
+    sortFieldPath: ['task', 'startDate'],
+    order: SortOrder.DESC,
+  },
+  {
+    sortName: 'responsible',
+    fieldType: SortFieldType.STRING,
+    sortFieldPath: ['responsible'],
+    order: SortOrder.DESC,
+  },
+]
+
 const theme = createMuiTheme(themeConfig)
 
 const renderComponent = async (props: SortBoxProps<TestDataType>) => {
@@ -169,13 +156,14 @@ const renderComponent = async (props: SortBoxProps<TestDataType>) => {
   return component
 }
 
-const createComponent = async (setSortedDataSpy, initialSort) => await renderComponent({
-  data: testList,
-  sortFields,
-  defaultSortFieldName: initialSort,
-  setSortedData: setSortedDataSpy,
-  labelPrefix: 'Sort by',
-})
+const createComponent = async (setSortedDataSpy, initialSort, sortFields = sortFieldsAsc) =>
+  await renderComponent({
+    data: testList,
+    sortFields,
+    defaultSortFieldName: initialSort,
+    setSortedData: setSortedDataSpy,
+    labelPrefix: 'Sort by',
+  })
 
 describe('SortBox component', () => {
   it('default sort - should return unsorted input list on first render', async () => {
@@ -185,31 +173,67 @@ describe('SortBox component', () => {
     expect(setSortedDataSpy).to.have.been.calledWith(testList)
   })
 
-  it('name sort - should return the input list sorted by name on first render', async () => {
-    const setSortedDataSpy = sinon.spy()
-    await createComponent(setSortedDataSpy, 'task name')
+  context('sort order ASC', async () => {
+    it('name sort - should return the input list sorted by name on first render', async () => {
+      const setSortedDataSpy = sinon.spy()
+      await createComponent(setSortedDataSpy, 'task name')
 
-    expect(setSortedDataSpy).to.have.been.calledWith(testListSortedByTaskName)
+      expect(setSortedDataSpy).to.have.been.calledWith(testListSortedByTaskNameAsc)
+    })
+
+    it('date sort - should return the input list sorted by task name on first render', async () => {
+      const setSortedDataSpy = sinon.spy()
+      await createComponent(setSortedDataSpy, 'start date')
+
+      expect(setSortedDataSpy).to.have.been.calledWith(testListSortedByTaskStartDateAsc)
+    })
+
+    it('change sort - should return the input list sorted by name', async () => {
+      const setSortedDataSpy = sinon.spy()
+      const component = await createComponent(setSortedDataSpy, defaultSort.sortName)
+
+      const selectComponent = component.find(testIdProp('sort-box')).first()
+      expect(selectComponent).to.exist
+      if (selectComponent && selectComponent.props() && selectComponent.props().onChange) {
+        selectComponent.props().onChange({ target: { value: 'task name' } })
+        await delay(150)
+        await settleComponent(component)
+      }
+      expect(setSortedDataSpy).to.have.been.calledWith(testListSortedByTaskNameAsc)
+    })
   })
 
-  it('date sort - should return the input list sorted by task name on first render', async () => {
-    const setSortedDataSpy = sinon.spy()
-    await createComponent(setSortedDataSpy, 'start date')
+  context('sort order DESC', async () => {
+    it('name sort - should return the input list sorted by name on first render', async () => {
+      const setSortedDataSpy = sinon.spy()
+      await createComponent(setSortedDataSpy, 'task name', sortFieldsDesc)
 
-    expect(setSortedDataSpy).to.have.been.calledWith(testListSortedByTaskStartDate)
-  })
+      expect(setSortedDataSpy).to.have.been.calledWith(testListSortedByTaskNameDesc)
+    })
 
-  it('change sort - should return the input list sorted by name', async () => {
-    const setSortedDataSpy = sinon.spy()
-    const component = await createComponent(setSortedDataSpy, defaultSort.sortName)
+    it('date sort - should return the input list sorted by task name on first render', async () => {
+      const setSortedDataSpy = sinon.spy()
+      await createComponent(setSortedDataSpy, 'start date', sortFieldsDesc)
 
-    const selectComponent = component.find(testIdProp('sort-box')).first()
-    expect(selectComponent).to.exist
-    if (selectComponent && selectComponent.props() && selectComponent.props().onChange) {
-      selectComponent.props().onChange({ target: { value: 'task name' } })
-      await delay(150)
-      await settleComponent(component)
-    }
-    expect(setSortedDataSpy).to.have.been.calledWith(testListSortedByTaskName)
+      expect(setSortedDataSpy).to.have.been.calledWith(testListSortedByTaskStartDateDesc)
+    })
+
+    it('change sort - should return the input list sorted by name', async () => {
+      const setSortedDataSpy = sinon.spy()
+      const component = await createComponent(
+        setSortedDataSpy,
+        defaultSort.sortName,
+        sortFieldsDesc,
+      )
+
+      const selectComponent = component.find(testIdProp('sort-box')).first()
+      expect(selectComponent).to.exist
+      if (selectComponent && selectComponent.props() && selectComponent.props().onChange) {
+        selectComponent.props().onChange({ target: { value: 'task name' } })
+        await delay(150)
+        await settleComponent(component)
+      }
+      expect(setSortedDataSpy).to.have.been.calledWith(testListSortedByTaskNameDesc)
+    })
   })
 })
