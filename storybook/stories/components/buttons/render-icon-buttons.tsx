90a60ce6f118a2d7084b85cfbd335fc0c0636551
Christoph Vitz <c.vitz@itsonix.eu>
2020-10-02 11:53:15 +0200

    PA-6817: Add story for icon buttons

---
+++ storybook/stories/components/buttons/render-icon-buttons.tsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import Visibility from '@material-ui/icons/Visibility'
+import VisibilityOutlined from '@material-ui/icons/VisibilityOutlined'
+import VisibilityOff from '@material-ui/icons/VisibilityOff'
+import VisibilityOffOutlined from '@material-ui/icons/VisibilityOffOutlined'
+import { Grid, IconButton } from '@material-ui/core'
+
+const renderIconButtons = args => (
+  <Grid container direction="row" spacing={1}>
+    <Grid item xs={1} container direction="column" spacing={1}>
+      <Grid item>
+        <IconButton {...args} size="small">
+          <Visibility />
+        </IconButton>
+      </Grid>
+      <Grid item>
+        <IconButton {...args} size="small">
+          <VisibilityOutlined />
+        </IconButton>
+      </Grid>
+    </Grid>
+    <Grid item xs={1} container direction="column" spacing={1}>
+      <Grid item>
+        <IconButton {...args} size="small">
+          <VisibilityOff />
+        </IconButton>
+      </Grid>
+      <Grid item>
+        <IconButton {...args} size="small">
+          <VisibilityOffOutlined />
+        </IconButton>
+      </Grid>
+    </Grid>
+  </Grid>
+)
+
+export {
+  renderIconButtons,
+}
