a9dc551a627fbd20427fc83c216f7a452ae8c037
Christoph Vitz <c.vitz@itsonix.eu>
2020-10-05 08:17:37 +0200

    PA-6817: Add proper mocking of translations in storybook

--- storybook/stories/providers.jsx
+++ storybook/stories/providers.jsx
@@ -1,3 +1,5 @@
+import i18n from 'i18next'
+import { I18nextProvider, initReactI18next } from 'react-i18next'
 import { CssBaseline } from '@material-ui/core'
 import { MuiPickersUtilsProvider } from '@material-ui/pickers'
 import DateFnsUtils from '@date-io/date-fns'
@@ -10,13 +12,34 @@ import themeConfig from '../../src'
 
 const theme = createMuiTheme(themeConfig)
 
+i18n
+  .use(initReactI18next)
+  .init({
+    lng: 'en',
+    fallbackLng: 'en',
+
+    // have a common namespace used around the full app
+    ns: ['translations'],
+    defaultNS: 'translations',
+
+    debug: true,
+
+    interpolation: {
+      escapeValue: false, // not needed for react!!
+    },
+
+    resources: { en: { translations: {} } },
+  })
+
 // eslint-disable-next-line react/prop-types
 const Providers = ({ children }) => (
   <ThemeProvider theme={theme}>
-    <MuiPickersUtilsProvider utils={DateFnsUtils} locale={enGB}>
-      <CssBaseline />
-      { children }
-    </MuiPickersUtilsProvider>
+    <I18nextProvider i18n={i18n}>
+      <MuiPickersUtilsProvider utils={DateFnsUtils} locale={enGB}>
+        <CssBaseline />
+        { children }
+      </MuiPickersUtilsProvider>
+    </I18nextProvider>
   </ThemeProvider>
 )
 
