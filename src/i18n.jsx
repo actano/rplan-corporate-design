f8ef36c893f0b094f605d452c6e3c7eb9d60d7a3
Christoph Vitz <c.vitz@itsonix.eu>
2020-10-05 08:26:47 +0200

    PA-6817: Extract namespace generationo

--- src/i18n.jsx
+++ src/i18n.jsx
@@ -1,5 +1,7 @@
 import { useTranslation as originalUseTranslation } from 'react-i18next'
 import pkg from '../package.json'
 
+export const getNamespace = fileName => `corporate-design:${pkg.version}:${fileName}`
+
 export const useTranslation = (fileName = 'translations', ...args) =>
-  originalUseTranslation(`corporate-design:${pkg.version}:${fileName}`, ...args)
+  originalUseTranslation(getNamespace(fileName), ...args)
