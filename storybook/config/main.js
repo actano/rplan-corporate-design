0154eb9d655120bdddd272e1e97a0c7fa88c875c
Christoph Vitz <c.vitz@itsonix.eu>
2020-10-05 14:01:06 +0200

    PA-6817: Remove `legacy` stories from config

--- storybook/config/main.js
+++ storybook/config/main.js
@@ -1,7 +1,6 @@
 module.exports = {
   stories: [
     '../stories/**/*.stories.@(js|jsx|ts|tsx|mdx)',
-    '../stories/legacy/**/*.@(js|jsx|ts|tsx)',
   ],
   addons: [
     '@storybook/preset-typescript',
