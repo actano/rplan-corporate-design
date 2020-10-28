d41b26d78549f2e50002fc681618786048f6a3df
marcel <marcel.nicorici@gmail.com>
2020-10-28 18:01:28 +0200

    PA-7258 Add 24 px milestone icons to corporate design

---
+++ src/icons/milestone-constants.tsx
@@ -0,0 +1,7 @@
+export enum MilestoneIconType {
+    NORMAL,
+    FLEXIBLE,
+    WITH_BUFFER,
+    WITH_DELAY,
+    COMPLETED,
+  }
---
+++ src/icons/milestone-icon-24.tsx
@@ -0,0 +1,107 @@
+import React from 'react'
+import { SvgIcon } from '@material-ui/core'
+import { MilestoneIconType } from './milestone-constants'
+import { height } from '../../lib/components/user-avatar.d'
+
+interface MilestoneIcon24Props {
+  className?: string,
+  type: MilestoneIconType,
+  [otherProp: string]: any,
+}
+
+const getContent = (type: MilestoneIconType) => {
+  let result
+  switch (type) {
+    case MilestoneIconType.FLEXIBLE:
+      result = (
+        <>
+          <path
+            d="M11.5858 23.5858L2.41421 14.4142C1.63316 13.6332 1.63316 12.3668 2.41421 11.5858L11.5858 2.41421C12.3669 1.63316 13.6332 1.63317 14.4142 2.41422L23.5858 11.5858C24.3668 12.3668 24.3668 13.6332 23.5858 14.4142L14.4142 23.5858C13.6332 24.3668 12.3669 24.3668 11.5858 23.5858Z"
+            fill="white"
+            stroke="#7B9AFE"
+            strokeWidth="2"
+          />
+          <path
+            d="M12.2929 18.8787L7.12132 13.7071C6.73079 13.3166 6.73079 12.6834 7.12132 12.2929L12.2929 7.12132C12.6834 6.7308 13.3166 6.7308 13.7071 7.12132L18.8787 12.2929C19.2692 12.6834 19.2692 13.3166 18.8787 13.7071L13.7071 18.8787C13.3166 19.2692 12.6834 19.2692 12.2929 18.8787Z"
+            fill="white"
+            stroke="#7B9AFE"
+            strokeWidth="2"
+          />
+          <rect x="9.99999" y="5" width="6" height="15.5" rx="3" fill="white" />
+        </>
+      )
+      break
+
+    case MilestoneIconType.WITH_BUFFER:
+      result = (
+        <path
+          d="M11.5858 23.5858L2.41422 14.4142C1.63317 13.6332 1.63317 12.3668 2.41422 11.5858L11.5858 2.41421C12.3669 1.63317 13.6332 1.63317 14.4142 2.41422L23.5858 11.5858C24.3668 12.3668 24.3668 13.6332 23.5858 14.4142L14.4142 23.5858C13.6332 24.3668 12.3669 24.3668 11.5858 23.5858Z"
+          fill="white"
+          stroke="#40C7B9"
+          strokeWidth="2"
+        />
+      )
+      break
+
+    case MilestoneIconType.WITH_DELAY:
+      result = (
+        <path
+          d="M11.0858 23.5858L1.91422 14.4142C1.13317 13.6332 1.13317 12.3668 1.91422 11.5858L11.0858 2.41421C11.8669 1.63316 13.1332 1.63317 13.9142 2.41422L23.0858 11.5858C23.8669 12.3668 23.8669 13.6332 23.0858 14.4142L13.9142 23.5858C13.1332 24.3668 11.8669 24.3668 11.0858 23.5858Z"
+          fill="white"
+          stroke="#F66D6B"
+          strokeWidth="2"
+        />
+      )
+      break
+
+    case MilestoneIconType.COMPLETED:
+      result = (
+        <>
+          <path
+            d="M11.0858 23.5858L1.91422 14.4142C1.13317 13.6332 1.13317 12.3668 1.91422 11.5858L11.0858 2.41421C11.8669 1.63317 13.1332 1.63317 13.9142 2.41422L23.0858 11.5858C23.8668 12.3668 23.8668 13.6332 23.0858 14.4142L13.9142 23.5858C13.1332 24.3668 11.8669 24.3668 11.0858 23.5858Z"
+            fill="white"
+            stroke="#7F869F"
+            strokeWidth="2"
+          />
+          <path
+            d="M17.25 10.705L11.25 16.705L8.5 13.955L9.205 13.25L11.25 15.29L16.545 10L17.25 10.705Z"
+            fill="#7F869F"
+            stroke="#7F869F"
+          />
+        </>
+      )
+      break
+
+    default:
+      result = (
+        <path
+          d="M11.0858 23.5858L1.91422 14.4142C1.13317 13.6332 1.13317 12.3668 1.91422 11.5858L11.0858 2.41421C11.8669 1.63316 13.1332 1.63317 13.9142 2.41421L23.0858 11.5858C23.8669 12.3668 23.8669 13.6332 23.0858 14.4142L13.9142 23.5858C13.1332 24.3668 11.8669 24.3668 11.0858 23.5858Z"
+          fill="white"
+          stroke="#7F869F"
+          strokeWidth="2"
+        />
+      )
+  }
+  return result
+}
+
+const MilestoneIcon24 = React.forwardRef<any, MilestoneIcon24Props>(({
+  className,
+  type = MilestoneIconType.NORMAL,
+  ...otherProps
+}, ref) => (
+  <SvgIcon
+    {...otherProps}
+    ref={ref}
+    className={className}
+    width="26"
+    height="26"
+    viewBox="0 0 26 26"
+  >
+    {
+      getContent(type)
+    }
+  </SvgIcon>
+))
+
+export { MilestoneIcon24, MilestoneIconType }
