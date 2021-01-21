bef2a31bc4874ffc337b10ec6c6452caea3c7a0c
HolgerWeber <holger.weber@actano.de>
2021-01-21 16:29:53 +0100

    PA-8399: Add default expansion icon
    - goal is to have a unified expansion icon for accordions instead of passing in the icon

--- src/components/non-collapsing-accordion-summary.tsx
+++ src/components/non-collapsing-accordion-summary.tsx
@@ -1,25 +1,43 @@
+import omit from 'lodash/omit'
 import React from 'react'
 import PropTypes from 'prop-types'
 import classnames from 'classnames'
 import { AccordionSummary, makeStyles } from '@material-ui/core'
+import ExpandIcon from '@material-ui/icons/ChevronRight'
 
-const useStyles = makeStyles(theme => ({
-  root: {
-    minHeight: theme.spacing(8),
-  },
-}))
+const useStyles = makeStyles((theme) => {
+  const transition = {
+    duration: theme.transitions.duration.shortest,
+  }
+  return {
+    root: {
+      minHeight: theme.spacing(8),
+    },
+    button: {
+      transform: 'rotate(0deg)',
+      transition: theme.transitions.create('transform', transition),
+      '&$expanded': {
+        transform: 'rotate(90deg)',
+      },
+    },
+    expanded: {},
+  }
+})
 
 interface PropTypes {
   className?: string,
+  showExpandIcon?: boolean,
   [key: string]: unknown,
 }
 
+const filterProps = props => omit(props, 'expandIcon')
 /*
   In contrast to the default expansion panel, this panel does not reduce its height when collapsed.
  */
 const NonCollapsingAccordionSummary: React.FC<PropTypes> = ({
   children,
   className,
+  showExpandIcon = true,
   ...props
 }) => {
   const classes = useStyles()
@@ -27,7 +45,12 @@ const NonCollapsingAccordionSummary: React.FC<PropTypes> = ({
   return (
     <AccordionSummary
       className={classnames(classes.root, className)}
-      {...props}
+      classes={{
+        expandIcon: classes.button,
+        expanded: classes.expanded,
+      }}
+      expandIcon={showExpandIcon ? <ExpandIcon /> : null}
+      {...filterProps(props)}
     >
       { children }
     </AccordionSummary>
