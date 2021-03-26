fc5e2312c664572bf35bacbf95140a295a76799d
Christoph Vitz <c.vitz@itsonix.eu>
2021-03-26 08:39:31 +0100

    PA-9427: Fix disabling dates in date picker

--- src/date-pickers/date-picker.jsx
+++ src/date-pickers/date-picker.jsx
@@ -1,3 +1,4 @@
+import { LocalDate, nativeJs } from '@js-joda/core'
 import React, { useCallback, useContext } from 'react'
 import PropTypes from 'prop-types'
 import { DatePicker as DatePickerMui, Day, MuiPickersContext } from '@material-ui/pickers'
@@ -34,29 +35,42 @@ const DatePicker = ({
   leftArrowButtonProps,
   rightArrowButtonProps,
   renderDay,
+  maxDate,
+  minDate,
   ...props
 }) => {
   const [translate] = useTranslation()
   const dateUtils = useContext(MuiPickersContext)
   const classes = useStyles()
 
-  const renderDayMerged = renderDay || ((date, selectedDate) => (
-    <div className={classes.container}>
-      {getDay(date) === 1 && (
-        <CommonTooltip title={translate('Calendar week')}>
-          <div className={classes.week}>
-            {getISOWeek(date)}
-          </div>
-        </CommonTooltip>
-      )}
-      <Day
-        current={isToday(date)}
-        selected={isSameDay(date, selectedDate)}
-      >
-        {getDate(date)}
-      </Day>
-    </div>
-  ))
+  const maxDateJoda = maxDate != null ? LocalDate.parse(maxDate) : null
+  const minDateJoda = minDate != null ? LocalDate.parse(minDate) : null
+
+  const renderDayMerged = renderDay || ((date, selectedDate) => {
+    const dateJoda = LocalDate.from(nativeJs(date))
+
+    return (
+      <div className={classes.container}>
+        {getDay(date) === 1 && (
+          <CommonTooltip title={translate('Calendar week')}>
+            <div className={classes.week}>
+              {getISOWeek(date)}
+            </div>
+          </CommonTooltip>
+        )}
+        <Day
+          current={isToday(date)}
+          selected={isSameDay(date, selectedDate)}
+          disabled={
+            (maxDateJoda && dateJoda.isAfter(maxDateJoda))
+            || (minDateJoda && dateJoda.isBefore(minDateJoda))
+          }
+        >
+          {getDate(date)}
+        </Day>
+      </div>
+    )
+  })
 
   const _renderDay = useCallback(
     (day, selectedDate, dayInCurrentMonth, dayComponent) => {
@@ -96,12 +110,16 @@ DatePicker.propTypes = {
   leftArrowButtonProps: PropTypes.objectOf(PropTypes.string),
   rightArrowButtonProps: PropTypes.objectOf(PropTypes.string),
   renderDay: PropTypes.func,
+  maxDate: PropTypes.string,
+  minDate: PropTypes.string,
 }
 
 DatePicker.defaultProps = {
   leftArrowButtonProps: {},
   rightArrowButtonProps: {},
   renderDay: undefined,
+  maxDate: undefined,
+  minDate: undefined,
 }
 
 export {
